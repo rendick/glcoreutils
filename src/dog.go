d4c6191dcc4af21c4d29115eec36f878bb679036
rendick <rendick123@proton.me>
2024-01-11 20:52:18 +0200

    dog

--- src/dog.go
+++ src/dog.go
@@ -7,5 +7,15 @@ import (
 
 func main() {
 	x := os.Args[1]
-	fmt.Println(x)
+
+	if x == "--help" {
+		fmt.Println("ealjfkhl")
+	}
+
+	file, err := os.Open(x)
+	if err != nil {
+		fmt.Println("sklfjshlk")
+	}
+	defer file.Close()
+
 }
