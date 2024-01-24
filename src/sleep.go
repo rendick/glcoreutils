561723a2b3cacd2a61592f6ef94ee1f074c2ef87
rendick <rendick123@proton.me>
2024-01-24 19:23:29 +0200

    new: minutes support

--- src/sleep.go
+++ src/sleep.go
@@ -28,24 +28,43 @@ func help() {
 	fmt.Println("sleep: missing operand\nUsage: sleep [NUMBER or OPTION]\nTry --help for more information")
 }
 
-func main() {
-	if len(os.Args) != 2 {
-		go help()
-		time.Sleep(time.Millisecond)
-		os.Exit(0)
-	}
-	switch os.Args[1] {
-	case "--help", "-help", "-h", "--h":
-		fmt.Println(sleep_help)
-	case "--version", "-version", "-v", "--v":
-		fmt.Println(sleep_version)
-	default:
+func sleep_minute() {
+	args := os.Args[2]
+
+	switch args {
+	case "-m":
 		i, err := strconv.Atoi(os.Args[1])
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(2)
 		} else {
-			time.Sleep(time.Duration(i) * time.Second)
+			time.Sleep(time.Duration(i) * time.Minute)
 		}
+	}
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		go help()
+		time.Sleep(time.Millisecond)
+		os.Exit(0)
+	} else if len(os.Args) == 2 {
+
+		switch os.Args[1] {
+		case "--help", "-help", "-h", "--h":
+			fmt.Println(sleep_help)
+		case "--version", "-version", "-v", "--v":
+			fmt.Println(sleep_version)
+		default:
+			i, err := strconv.Atoi(os.Args[1])
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(2)
+			} else {
+				time.Sleep(time.Duration(i) * time.Second)
+			}
+		}
+	} else if len(os.Args) == 3 {
+		sleep_minute()
 	}
 }
